5f7853b47a9befb0adb27e98cf4a519779579431
banosarwari8928 <banosarwari8928@gmail.com>
2025-11-24 02:25:00 +0430

    Error handler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,17 @@
+import Link from "next/link";
 import React, { Children } from "react";
 
 export default function layout({children}:{children: React.ReactNode}){
   return(
     <html>
       <body>
-      <nav style={{backgroundColor:'skyblue',
-         boxShadow:'inherit',
-         height:'30px',
-         width:'100vw' ,
-         textAlign:'center',
-         justifyContent:'center'
-          }}>
-            This is my navbar
+      <nav style={{backgroundColor:'skyblue',  boxShadow:'inherit',height:'30px',width:'100vw' , textAlign:'center', justifyContent:'center' }}>
+    <Link href="/post" className="">Post</Link>
+        
             </nav>
-   <h1 
-   style={{
-     color:"green", 
-     fontFamily:"cursive", 
-     borderRadius:"5px" ,
-     padding:"4px",
-     textAlign:"center", 
-     alignItems:"center", 
-     width:"30%", 
-     margin:"auto 0px" 
-     }}>
-       Hi I am from layout</h1>
     <h1>{children}</h1>
     <footer 
-    style={{
-    backgroundColor:'yellowgreen',
-    boxShadow:"0.5px  0.9px 9px gray",
-    height:'30px',
-    width:'100vw' ,
-    textAlign:'center',
-    justifyContent:'center'
+    style={{ backgroundColor:'yellowgreen', boxShadow:"0.5px  0.9px 9px gray", height:'30px', width:'100vw' ,textAlign:'center',justifyContent:'center'
         }}>
     This is my Footer
           </footer>
---
+++ src/app/post/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from 'react'
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
+
+
+function error({error , reset}:{error:Error,reset:()=>void}) {
+   const router=useRouter();
+    function handleClick(){
+        router.refresh();
+        startTransition(()=>{
+            reset();
+        });
+    }
+  return (
+    <div>
+        <h1>{error.message}</h1>
+        <button onClick={()=>handleClick() } className='bg-red-400 text-white py-2 px-6 rounded'> Try again</button>
+    </div>
+  )
+}
+
+export default error;
