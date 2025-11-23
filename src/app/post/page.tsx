c5aea131044cb0d2ac6cdca8141d1e2fc25f6431
banosarwari8928 <banosarwari8928@gmail.com>
2025-11-24 03:13:11 +0430

    Error handler

---
+++ src/app/post/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function getRnum( num:number){
+    return Math.floor(Math.random()*num)
+}
+function page() {
+   const Rnum= getRnum(3);
+   if(Rnum<3){
+    throw new Error("NOT FOUND");
+   }
+  return (
+    <div className='text-2xl p-4 bg-red-800 h-fit w-fit'>This is Post page  </div>
+  )
+}
+
+export default page;
